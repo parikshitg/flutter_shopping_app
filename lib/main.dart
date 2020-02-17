759024171183e87daa0fb9b2aba8b9bdcbd9ff44
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2020-02-17 17:29:15 +0530

    providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,14 +11,20 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopping App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
+          child: MaterialApp(
+        title: 'Shopping App',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
