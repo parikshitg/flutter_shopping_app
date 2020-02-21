3dce8e8c74cb9df4c66dbe1bca6c4663e2f84003
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2020-02-21 15:15:34 +0530

    post form values edit product

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/providers/orders.dart';
 import 'package:flutter_shopping_app/screens/cart_screen.dart';
+import 'package:flutter_shopping_app/screens/edit_product_screen.dart';
 import 'package:flutter_shopping_app/screens/orders_screen.dart';
 import 'package:flutter_shopping_app/screens/user_products_screen.dart';
 import 'package:provider/provider.dart';
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName:(ctx) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_app/screens/edit_product_screen.dart';
 import 'package:flutter_shopping_app/widgets/app_drawer.dart';
 import 'package:flutter_shopping_app/widgets/user_product_item.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +15,12 @@ class UserProductsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Products'),
         actions: <Widget>[
-          IconButton(icon: const Icon(Icons.add), onPressed: null)
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
         ],
       ),
       drawer: AppDrawer(),
